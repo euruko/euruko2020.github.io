4073bc239ec8ce058a5e8ff14b29a763086077a2
Matias Korhonen <matias@kiskolabs.com>
2019-08-22 00:11:03 +0300

    Use terser to minify the production code

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const merge = require("webpack-merge");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
@@ -10,23 +10,21 @@ module.exports = merge(common, {
 
   output: {
     filename: "[name].[hash:5].js",
-    chunkFilename: "[id].[hash:5].css"
+    chunkFilename: "[id].[hash:5].css",
   },
 
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
+      new TerserPlugin({
         parallel: true,
-        sourceMap: true
       }),
 
       new MiniCssExtractPlugin({
         filename: "[name].[hash:5].css",
-        chunkFilename: "[id].[hash:5].css"
+        chunkFilename: "[id].[hash:5].css",
       }),
 
-      new OptimizeCSSAssetsPlugin({})
-    ]
-  }
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
 });
