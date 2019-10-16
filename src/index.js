741f1c91ff6ee42b62d89de903582771c0a21460
Teemu Rytilä <teemu@kiskolabs.com>
2019-10-16 23:41:36 +0300

    Add global/variable styles, header, and hero

--- src/index.js
+++ src/index.js
@@ -9,3 +9,25 @@ const app = new Form({
 });
 
 export default app;
+
+const header = document.getElementById("header");
+const mobileToggle = document.getElementById("mobile-toggle");
+const heroImage = document.getElementById("hero-image");
+const langSelect = document.getElementById("lang-select");
+
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > 100) {
+    header.classList.remove("header--not-scrolled");
+  } else {
+    header.classList.add("header--not-scrolled");
+  }
+  heroImage.style.cssText = "transform: translateY(" + (window.scrollY * 0.3).toFixed(0) + "px);";
+});
+
+mobileToggle.addEventListener("click", (e) => {
+  header.classList.toggle("header--mobile-toggled");
+});
+
+langSelect.addEventListener("click", (e) => {
+  header.classList.toggle("header--language-toggled");
+});
