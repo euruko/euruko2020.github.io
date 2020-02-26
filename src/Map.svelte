76ff55a72c629ffbb60056e87be0cc9ebc665410
Matias Korhonen <matias@kiskolabs.com>
2020-02-26 19:18:17 +0200

    Fetch the map data async

--- src/Map.svelte
+++ src/Map.svelte
@@ -1,41 +1,37 @@
 <script>
   import { onMount } from "svelte";
-  import attendees from "./data/countries.json";
-  import world from "./data/world.json";
+  // import attendees from "./data/countries.json";
+  // import world from "./data/world.json";
   import L from "leaflet";
   import chroma from "chroma-js";
 
+  let map;
+  let world;
   let leafletContainer;
+  let attendees;
 
-  const colorScale = chroma.scale(["#fff", "#f2f0f7", "#503069"])
-    .domain([0, 1, Math.max(...attendees.map(country => country.percentage))], 7, "log")
-    .mode("hsl");
+  function renderMap() {
+    const colorScale = chroma.scale(["#fff", "#f2f0f7", "#503069"])
+      .domain([0, 1, Math.max(...attendees.map(country => country.percentage))], 7, "log")
+      .mode("hsl");
 
-  function style(feature) {
-    return {
-      fillColor: colorScale(feature.properties.percentage).hex(),
-      weight: 1,
-      opacity: 1,
-      color: '#4F3767',
-      fillOpacity: 1
-    };
-  }
+    function style(feature) {
+      return {
+        fillColor: colorScale(feature.properties.percentage).hex(),
+        weight: 1,
+        opacity: 1,
+        color: '#4F3767',
+        fillOpacity: 1
+      };
+    }
 
-  const features = world.features.map((country) => {
-    const data = attendees.find((item) => item.alpha3 === country.id);
-    country.properties["percentage"] = data ? data.percentage : 0;
-    return country;
-  });
-
-  const filtered = features.filter(country => country.id !== "ATA");
-
-  onMount(() => {
-    let map = L.map(leafletContainer, {
-      center: [54.9, 25.316667],
-      zoom: 3.25,
-      attributionControl: false,
-      scrollWheelZoom: false
+    const features = world.features.map((country) => {
+      const data = attendees.find((item) => item.alpha3 === country.id);
+      country.properties["percentage"] = data ? data.percentage : 0;
+      return country;
     });
+
+    const filtered = features.filter(country => country.id !== "ATA");
 
     let geojson = L.geoJson(filtered, {
       style: style,
@@ -62,6 +58,31 @@
       }
     });
     geojson.addTo(map);
+  }
+
+  function getAttendees() {
+    const attendeesURL = document.body.getAttribute("data-countries");
+    return fetch(attendeesURL).then(response => response.json())
+  }
+
+  function getWorld() {
+    const worldURL = document.body.getAttribute("data-world");
+    return fetch(worldURL).then(response => response.json())
+  }
+
+  onMount(() => {
+    map = L.map(leafletContainer, {
+      center: [54.9, 25.316667],
+      zoom: 3.25,
+      attributionControl: false,
+      scrollWheelZoom: false
+    });
+
+    Promise.all([getAttendees(), getWorld()]).then(result => {
+      attendees = result[0];
+      world = result[1];
+      renderMap();
+    })
   });
 </script>
 
@@ -117,11 +138,13 @@
     </tr>
   </thead>
   <tbody>
-    {#each attendees.reverse() as country}
-    <tr>
-      <td>{country.name}</td>
-      <td>{country.percentage === 1 ? "≤1" : country.percentage}%</td>
-    </tr>
-    {/each}
+    {#if attendees}
+      {#each attendees.reverse() as country}
+      <tr>
+        <td>{country.name}</td>
+        <td>{country.percentage === 1 ? "≤1" : country.percentage}%</td>
+      </tr>
+      {/each}
+    {/if}
   </tbody>
 </table>
